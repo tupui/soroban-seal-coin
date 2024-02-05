8605c4a1e3bf0efa1a07638aa0d434c484ab8a58
Pamphile Roy <roy.pamphile@gmail.com>
2024-02-05 23:06:24 +0100

    Refactor tests

--- contract/src/test.rs
+++ contract/src/test.rs
@@ -1,20 +1,10 @@
 #![cfg(test)]
 
 use super::{SealCoinContract, SealCoinContractClient};
-use soroban_sdk::testutils::{Address as _};
+use soroban_sdk::testutils::Address as _;
 use soroban_sdk::{token, Address, Env};
 use token::Client as TokenClient;
 use token::StellarAssetClient as TokenAdminClient;
-
-
-fn create_token_contract<'a>(e: &Env, admin: &Address) -> (TokenClient<'a>, TokenAdminClient<'a>) {
-    let contract_address = e.register_stellar_asset_contract(admin.clone());
-    (
-        TokenClient::new(e, &contract_address),
-        TokenAdminClient::new(e, &contract_address),
-    )
-}
-
 
 #[test]
 fn test() {
@@ -27,24 +17,30 @@ fn test() {
     let issuer = Address::generate(&env);
     let contract_admin = Address::generate(&env);
 
-    let (token, issuer_client) = create_token_contract(&env, &issuer);
+    let token_address = env.register_stellar_asset_contract(issuer.clone());
+    let token = TokenClient::new(&env, &token_address);
+    let issuer_client = TokenAdminClient::new(&env, &token_address);
+
+    assert_eq!(token.balance(&distributor), 0);
     issuer_client.mint(&distributor, &1_000_000_000);
     assert_eq!(token.balance(&distributor), 1_000_000_000);
 
     let contract_id = env.register_contract(None, SealCoinContract);
     let contract = SealCoinContractClient::new(&env, &contract_id);
 
-    contract.init(&contract_admin, &token.address);
+    contract.init(&contract_admin, &distributor);
 
-    // doy 0 -> 13.823
+    // doy 1 -> 13.823
+    // 10K diff -> 1M token diff
     // Mint
-    contract.update_sea_ice_extent(&issuer, &distributor, &0, &23823);
-    assert_eq!(token.balance(&distributor), 1_000_010_000);
+    contract.update_sea_ice_extent(&issuer, &distributor, &1, &23823);
+    assert_eq!(token.balance(&distributor), 1_001_000_000);
 
-    contract.update_sea_ice_extent(&issuer, &distributor, &0, &14823);
-    assert_eq!(token.balance(&distributor), 1_000_010_000);
+    // increment too small <1000
+    contract.update_sea_ice_extent(&issuer, &distributor, &1, &14823);
+    assert_eq!(token.balance(&distributor), 1_001_000_000);
 
     // Burn
-    contract.update_sea_ice_extent(&issuer, &distributor, &0, &3823);
+    contract.update_sea_ice_extent(&issuer, &distributor, &1, &3823);
     assert_eq!(token.balance(&distributor), 1_000_000_000);
 }
